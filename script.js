74046839c1f61a129318e2201fdfa637512e3133
camfpli2 <37838007+camfpli2@users.noreply.github.com>
2025-06-19 11:02:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ var whichLabels;
 var whichTicks;
 
 function preload(){
-   // data = loadTable("1ex60beans.csv", "csv");
-   data = loadTable("greenhouse-gas-emissions.csv", "csv");
+    data = loadTable("1ex60beans.csv", "csv");
+  // data = loadTable("greenhouse-gas-emissions.csv", "csv");
 
 }
 
@@ -31,7 +31,7 @@ function setup() {          //this function runs once upon startup
   controls[1]=new control(170,5,160,72,"Enter Your Own",true,butColor);
   controls[2]=new control(1200,150,80,48,"DEC.",true,butColor);
   controls[3]=new control(1300,150,80,48,"INC.",true,butColor);
-  dataset=data.getColumn(4);
+  dataset=data.getColumn(0);
   var possTitle=isNumber(dataset[0]);
   if (possTitle===false){
        dtitle = dataset.shift();
