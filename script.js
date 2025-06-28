2dc683f1d256d4f05a69fee5c744bad462bbce32
camfpli2 <37838007+camfpli2@users.noreply.github.com>
2025-06-28 12:25:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function setup() {          //this function runs once upon startup
 
 function processData(){
     for(var g=0;g<dataset.length;g++){
-        if(isNumber(dataset[g])===false){
+        if(dataset[g]===''){
             dataset.splice(g,1);
         }
     }
